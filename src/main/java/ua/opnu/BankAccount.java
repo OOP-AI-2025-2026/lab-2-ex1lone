8849261d91bff9364d9dcc4280d6036c7a8e3fa1
ex1lone <staskushnir4567@gmail.com>
2025-10-06 15:51:05 +0300

    Update BankAccount.java

--- src/main/java/ua/opnu/BankAccount.java
+++ src/main/java/ua/opnu/BankAccount.java
@@ -1,82 +1,50 @@
 package ua.opnu;
 
 public class BankAccount {
-    private String name;
-    private double balance;
-    private double transactionFee = 0.0; // початкове значення комісії
+  String name;
+  double balance;
+  double transactionFee = 0;
 
-    // Конструктор
-    public BankAccount(String name, double balance) {
-        if (balance < 0) {
-            throw new IllegalArgumentException("Баланс не може бути від’ємним");
-        }
-        this.name = name;
-        this.balance = balance;
+  void deposit(double amount) {
+    // TODO: modify method body
+
+    if (amount < 0) {
+      return;
     }
 
-    // Встановити комісію
-    public void setTransactionFee(double fee) {
-        if (fee < 0) {
-            throw new IllegalArgumentException("Комісія не може бути від’ємною");
-        }
-        this.transactionFee = fee;
+    balance = balance + amount;
+  }
+
+  double getBalance() {
+    return this.balance;
+  }
+
+  boolean withdraw(double amount) {
+    // TODO: modify method body
+    double a;
+    a = balance - amount - transactionFee;
+
+    if (a < 0 || amount <= 0) {
+      return false;
     }
 
-    // Отримати комісію
-    public double getTransactionFee() {
-        return transactionFee;
+    balance = a;
+
+    return true;
+  }
+
+  boolean transfer(BankAccount receiver, double amount) {
+    // TODO: modify method body
+
+    double a = this.balance - amount - transactionFee;
+    if (a < 0 || amount <= 0) {
+      return false;
     }
 
-    // Поповнення рахунку
-    public void deposit(double amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("Сума поповнення повинна бути більше 0");
-        }
-        balance += amount;
-    }
+    this.balance = a;
 
-    // Отримання поточного балансу
-    public double getBalance() {
-        return balance;
-    }
+    receiver.deposit(amount);
 
-    // Зняття коштів (із врахуванням комісії)
-    public boolean withdraw(double amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("Сума зняття повинна бути більше 0");
-        }
-
-        double total = amount + transactionFee;
-        if (total > balance) {
-            return false; // недостатньо коштів, баланс не змінюємо
-        }
-
-        balance -= total;
-        return true;
-    }
-
-    // Переказ коштів іншому рахунку
-    public boolean transfer(BankAccount receiver, double amount) {
-        if (receiver == null) {
-            throw new IllegalArgumentException("Отримувач не може бути null");
-        }
-        if (amount <= 0) {
-            throw new IllegalArgumentException("Сума переказу повинна бути більше 0");
-        }
-
-        double total = amount + transactionFee;
-        if (total > balance) {
-            return false; // недостатньо коштів
-        }
-
-        balance -= total;      // списуємо з поточного рахунку суму + комісію
-        receiver.balance += amount; // зараховуємо суму на рахунок отримувача без комісії
-
-        return true;
-    }
-
-    // Отримати ім'я власника рахунку
-    public String getName() {
-        return name;
-    }
+    return true;
+  }
 }
