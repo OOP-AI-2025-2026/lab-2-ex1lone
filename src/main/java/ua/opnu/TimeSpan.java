9cef816cf06593603023474dd8593fcadb08ef92
ex1lone <staskushnir4567@gmail.com>
2025-10-06 15:31:16 +0300

    Update TimeSpan.java

--- src/main/java/ua/opnu/TimeSpan.java
+++ src/main/java/ua/opnu/TimeSpan.java
@@ -1,46 +1,80 @@
 package ua.opnu;
 
-
 public class TimeSpan {
 
-    // TODO: add class fields
+    private int hours;
+    private int minutes;
 
+    // Конструктор
     TimeSpan(int hours, int minutes) {
-        // TODO: write constructor body
+        if (hours < 0 || minutes < 0 || minutes > 59) {
+            throw new IllegalArgumentException("Некоректні години або хвилини");
+        }
+        this.hours = hours;
+        this.minutes = minutes;
     }
 
+    // Повертає кількість годин
     int getHours() {
-        return 0;
+        return hours;
     }
 
+    // Повертає кількість хвилин
     int getMinutes() {
-        // TODO: write method body
-        return 0;
+        return minutes;
     }
 
+    // Додавання годин і хвилин до інтервалу
     void add(int hours, int minutes) {
-        // TODO: write method body
+        if (hours < 0 || minutes < 0 || minutes > 59) {
+            throw new IllegalArgumentException("Некоректні години або хвилини");
+        }
+
+        int totalMinutes = this.minutes + minutes;
+        int totalHours = this.hours + hours + totalMinutes / 60;
+        totalMinutes %= 60;
+
+        this.hours = totalHours;
+        this.minutes = totalMinutes;
     }
 
+    // Додавання іншого TimeSpan
     void addTimeSpan(TimeSpan timespan) {
-        // TODO: write method body
+        add(timespan.getHours(), timespan.getMinutes());
     }
 
+    // Загальна кількість годин (у вигляді дробового числа)
     double getTotalHours() {
-        // TODO: write method body
-        return 0;
+        return hours + minutes / 60.0;
     }
 
+    // Загальна кількість хвилин
     int getTotalMinutes() {
-        // TODO: write method body
-        return 0;
+        return hours * 60 + minutes;
     }
 
+    // Віднімання іншого інтервалу
     void subtract(TimeSpan span) {
-        // TODO: write method body
+        int totalThis = getTotalMinutes();
+        int totalOther = span.getTotalMinutes();
+
+        if (totalOther > totalThis) {
+            throw new IllegalArgumentException("Неможливо відняти більший інтервал від меншого");
+        }
+
+        int result = totalThis - totalOther;
+        this.hours = result / 60;
+        this.minutes = result % 60;
     }
 
+    // Масштабування інтервалу
     void scale(int factor) {
-        // TODO: write method body
+        if (factor <= 0) {
+            throw new IllegalArgumentException("Множник має бути більше 0");
+        }
+
+        int totalMinutes = getTotalMinutes() * factor;
+        this.hours = totalMinutes / 60;
+        this.minutes = totalMinutes % 60;
     }
 }
